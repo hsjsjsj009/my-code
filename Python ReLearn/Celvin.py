f61910e59f7732829bf8e5870ee1f076ee2e8ee1
VanLau <dipta004@gmail.com>
2019-05-24 14:09:10 +0700

    temp

---
+++ Python ReLearn/Celvin.py
@@ -0,0 +1,11 @@
+inputan  = input()
+inputan = inputan.split()
+hour = int(inputan[0])
+mins = int(inputan[1])
+duration = int(inputan[2])
+
+minsTemp = (duration + mins) % 60
+hour = (hour + ((duration + mins) // 60)) % 24
+
+
+print("{}:{}".format(hour,minsTemp))    
